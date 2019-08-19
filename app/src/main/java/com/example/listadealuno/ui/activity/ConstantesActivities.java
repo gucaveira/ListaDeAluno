d66244820b3820bd6b4419e2007e8f5800efdd9a
gustavo <pereiragustavo215@gmail.com>
2019-08-19 13:02:12 -0300

    add a classe FormularioAluno e xml

---
+++ app/src/main/java/com/example/listadealuno/ui/activity/ConstantesActivities.java
@@ -0,0 +1,5 @@
+package com.example.listadealuno.ui.activity;
+
+public interface ConstantesActivities {
+    String CHAVE_ALUNO = "aluno";
+}
---
+++ app/src/main/java/com/example/listadealuno/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,99 @@
+package com.example.listadealuno.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.listadealuno.R;
+import com.example.listadealuno.dao.AlunoDao;
+import com.example.listadealuno.model.Aluno;
+
+import static com.example.listadealuno.ui.activity.ConstantesActivities.CHAVE_ALUNO;
+
+public class FormularioAlunoActivity  extends AppCompatActivity {
+
+  private static final String TITULO_APPBAR_NOVO_ALUNO = "Novo aluno";
+  private static final String TITULO_APPCAR_EDITA_ALUNO = "Edita aluno";
+  private EditText campoNome;
+  private EditText campoTelefone;
+  private EditText campoEmail;
+  private final AlunoDao dao = new AlunoDao();
+  private Aluno aluno;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_formulario_aluno);
+    inicializacaoDosCampos();
+    CarregaAluno();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater()
+            .inflate(R.menu.activity_formulario_aluno_menu, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    int itemId = item.getItemId();
+    if(itemId == R.id.activity_fomulario_aluno_salvar){
+      finalizaFormulario();
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void CarregaAluno() {
+    Intent dados = getIntent();
+    if (dados.hasExtra(CHAVE_ALUNO)){
+      setTitle(TITULO_APPCAR_EDITA_ALUNO);
+      aluno = (Aluno) dados.getSerializableExtra(CHAVE_ALUNO);
+      preencheCampos();
+    }else {
+      setTitle(TITULO_APPBAR_NOVO_ALUNO);
+      aluno = new Aluno();
+    }
+  }
+
+  private void preencheCampos() {
+    campoNome.setText(aluno.getNome());
+    campoTelefone.setText(aluno.getTelefone());
+    campoEmail.setText(aluno.getEmail());
+
+  }
+  private void finalizaFormulario() {
+    preencheAluno();
+    if(aluno.temIdValido()){
+        dao.edita(aluno);
+    }else {
+      dao.salva(aluno);
+    }
+    finish();
+  }
+
+
+  private void inicializacaoDosCampos() {
+    campoNome = findViewById(R.id.activity_fomulario_aluno_nome);
+    campoTelefone = findViewById(R.id.activity_fomulario_aluno_telefone);
+    campoEmail = findViewById(R.id.activity_fomulario_aluno_email);
+  }
+
+  private void preencheAluno() {
+    String nome = campoNome.getText().toString();
+    String telefone = campoTelefone.getText().toString();
+    String email = campoEmail.getText().toString();
+
+    aluno.setNome(nome);
+    aluno.setTelefone(telefone);
+    aluno.setEmail(email);
+  }
+
+
+}
